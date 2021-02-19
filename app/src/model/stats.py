5282fad32efb5f63d963ecaa49c57d89f058b1b8
gescudero <gescudero@gmail.com>
2021-02-19 13:50:15 +0100

    New Stats & StatsItem classes

---
+++ app/src/model/stats.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+from .stats_item import StatsItem
+class Stats():
+    def __init__(self, _cfg):
+        self.cfg = _cfg
+        self.overall = []
+        self.solo = []
+        self.duos = []
+        self.squad = []
+
+    
+    def fill_from_file(filename):
+        try:
+            with open(filename, 'r') as f:
+                dict_stats = json.loads(f.read())
+            if dict_stats:
+                for s in dict_stats["overall"]:
+                    self.overall.append(StatsItem.from_dict(s))
+                #self.overall = [s.from_trn_dict() for s in dict_stats["overall"]]
+                for s in dict_stats["solo"]:
+                    self.solo.append(StatsItem.from_dict(s))
+                for s in dict_stats["duos"]:
+                    self.duos.append(StatsItem.from_dict(s))
+                for s in dict_stats["squad"]:
+                    self.squad.append(StatsItem.from_dict(s))
+        except FileNotFoundError:
+            #Not found, pues nada, pasamos del tema
+            println("No hay fichero de stats")
+    
+
+    def add_stats(self, game_mode, trn_data):
+        if game_mode == "solo":
+            self.solo.append(StatsItem.from_trn_dict("solo", trn_data))
+        elif game_mode == "duos":
+            self.duos.append(StatsItem.from_trn_dict("duos", trn_data))
+        elif game_mode == "squad":
+            self.squad.append(StatsItem.from_trn_dict("squad", trn_data))
+
+    
+    def add_stats_from_trn_dict(self, trn_data):
+        gap = self.cfg["sessions"]["gapBetweenSessions"] # gap en segundos
+        last_match_date = datetime.strptime(trn_data["recentMatches"][0]["dateCollected"], "%Y-%m-%dT%H:%M:%S")
+        now = datetime.now()
+        interval = abs(now - last_match_date).seconds
+
+        # solo actualizamos stats si hemos cambiado de sesion y hay datos nuevos
+        if interval > gap:
+            new_data = False
+            if self.solo:
+                if trn_data["stats"]["p2"]["matches"]["valueInt"] > self.solo[-1].matches:
+                    self.solo.append(StatsItem.from_trn_dict("solo", trn_data))
+                    new_data = True
+            else:
+                self.solo.append(StatsItem.from_trn_dict("solo", trn_data))
+                new_data = True
+            
+            if self.duos:
+                if trn_data["stats"]["p10"]["matches"]["valueInt"] > self.duos[-1].matches:
+                    self.duos.append(StatsItem.from_trn_dict("duos", trn_data))
+                    new_data = True
+            else:
+                self.duos.append(StatsItem.from_trn_dict("duos", trn_data))
+                new_data = True
+
+            if self.squad:
+                if trn_data["stats"]["p9"]["matches"]["valueInt"] > self.squad[-1].matches:
+                    self.squad.append(StatsItem.from_trn_dict("squad", trn_data))
+                    new_data = True
+            else:
+                self.squad.append(StatsItem.from_trn_dict("squad", trn_data))
+                new_data = True
+            
+            if new_data:
+                self.overall.append(self.get_new_overall())
+
+
+    def get_new_overall(self):
+        new_stats_item = StatsItem()
+        #self.overall.trn_rating = 
+        new_stats_item.score = self.solo[-1].score + self.duos[-1].score + self.squad[-1].score
+        new_stats_item.top1 = self.solo[-1].top1 + self.duos[-1].top1 + self.squad[-1].top1
+        new_stats_item.top3 = self.solo[-1].top3 + self.duos[-1].top3 + self.squad[-1].top3
+        new_stats_item.top5 = self.solo[-1].top5 + self.duos[-1].top5 + self.squad[-1].top5
+        new_stats_item.top6 = self.solo[-1].top6 + self.duos[-1].top6 + self.squad[-1].top6
+        new_stats_item.top10 = self.solo[-1].top10 + self.duos[-1].top10 + self.squad[-1].top10
+        new_stats_item.top12 = self.solo[-1].top12 + self.duos[-1].top12 + self.squad[-1].top12
+        new_stats_item.top25 = self.solo[-1].top25 + self.duos[-1].top25 + self.squad[-1].top25
+        new_stats_item.kd = (self.solo[-1].kd + self.duos[-1].kd + self.squad[-1].kd) / 3
+        new_stats_item.matches = self.solo[-1].matches + self.duos[-1].matches + self.squad[-1].matches
+        new_stats_item.kills = self.solo[-1].kills + self.duos[-1].kills + self.squad[-1].kills
+        new_stats_item.minutes_played = self.solo[-1].minutes_played + self.duos[-1].minutes_played + self.squad[-1].minutes_played
+        now = datetime.now()
+        new_stats_item.date = now.strftime("%Y-%m-%dT%H:%M:%S")
+        return new_stats_item
+
+
+    def get_dict(self):
+        new_dict = {
+            "overall": {},
+            "solo": {},
+            "duos": {},
+            "squad": {}
+        }
+        new_dict["overall"] = [s.get_dict() for s in self.overall]
+        new_dict["solo"] = [s.get_dict() for s in self.solo]
+        new_dict["duos"] = [s.get_dict() for s in self.duos]
+        new_dict["squad"] = [s.get_dict() for s in self.squad]
+        return new_dict
---
+++ app/src/model/stats_item.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+
+class StatsItem():
+    def __init__(self):
+        self.game_mode = ""
+        self.trn_rating = 0
+        self.score = 0
+        self.top1 = 0
+        self.top3 = 0
+        self.top5 = 0
+        self.top6 = 0
+        self.top10 = 0
+        self.top12 = 0
+        self.top25 = 0
+        self.kd = 0
+        self.matches = 0
+        self.kills = 0
+        self.minutes_played = 0
+        self.date = ""
+
+
+    @staticmethod
+    def from_dict(dict):
+        new_item = StatsItem()
+        new_item.game_mode = dict["game_mode"]
+        new_item.trn_rating = dict["trn_rating"]
+        new_item.score = dict["score"]
+        new_item.top1 = dict["top1"]
+        new_item.top3 = dict["top3"]
+        new_item.top5 = dict["top5"]
+        new_item.top6 = dict["top6"]
+        new_item.top1 = dict["top1dict"]
+        new_item.top12 = dict["top12"]
+        new_item.top25 = dict["top25"]
+        new_item.kd = dict["kd"]
+        new_item.matches = dict["matches"]
+        new_item.kills = dict["kills"]
+        new_item.minutes_played = dict["minutes_played"]
+        new_item.date = dict["date"]
+        return new_item
+
+    @staticmethod 
+    def from_trn_dict(game_mode, trn_stats):
+        new_item = StatsItem()
+        new_item.game_mode = game_mode
+
+        if game_mode == "solo":
+            trn_playlist = "p2"
+        elif game_mode == "duos":
+            trn_playlist = "p10"
+        elif game_mode == "squad":
+            trn_playlist = "p9"
+        else:
+            return new_item
+
+        new_item.trn_rating = trn_stats["stats"][trn_playlist]["trnRating"]["valueInt"]
+        new_item.score = trn_stats["stats"][trn_playlist]["score"]["valueInt"]
+        new_item.top1 = trn_stats["stats"][trn_playlist]["top1"]["valueInt"]
+        new_item.top3 = trn_stats["stats"][trn_playlist]["top3"]["valueInt"]
+        new_item.top5 = trn_stats["stats"][trn_playlist]["top5"]["valueInt"]
+        new_item.top6 = trn_stats["stats"][trn_playlist]["top6"]["valueInt"]
+        new_item.top10 = trn_stats["stats"][trn_playlist]["top10"]["valueInt"]
+        new_item.top12 = trn_stats["stats"][trn_playlist]["top12"]["valueInt"]
+        new_item.top25 = trn_stats["stats"][trn_playlist]["top25"]["valueInt"]
+        new_item.kd = trn_stats["stats"][trn_playlist]["kd"]["valueDec"]
+        new_item.matches = trn_stats["stats"][trn_playlist]["matches"]["valueInt"]
+        new_item.kills = trn_stats["stats"][trn_playlist]["kills"]["valueInt"]
+        new_item.minutes_played = trn_stats["stats"][trn_playlist]["minutesPlayed"]["valueInt"]
+        now = datetime.now()
+        new_item.date = now.strftime("%Y-%m-%dT%H:%M:%S")
+
+        return new_item
+
+
+    def get_dict(self):
+        new_dict = {
+            "game_mode": self.game_mode,
+            "trn_rating": self.trn_rating,
+            "score": self.score,
+            "top1": self.top1,
+            "top3": self.top3,
+            "top5": self.top5,
+            "top6": self.top6,
+            "top10": self.top10,
+            "top12": self.top12,
+            "top25": self.top25,
+            "kd": self.kd,
+            "matches": self.matches,
+            "kills": self.kills,
+            "minutes_played": self.minutes_played,
+            "date": self.date
+        }
+        return new_dict
+
+
+
+        
