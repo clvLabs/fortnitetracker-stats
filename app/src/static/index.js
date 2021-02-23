bfea98e2f6b52861fedbe361d388375f4a2270c2
Guillermo Escudero <guillermoescudero@dorna.com>
2021-02-23 16:56:15 +0100

    web ui tests

--- app/src/static/index.js
+++ app/src/static/index.js
@@ -26,37 +26,14 @@ function onSampleButton() {
     toast("Sample button pressed", TOAST_OK);
 }
 
-function onActivateProfile(profile_name)
+function onSelectProfile(profile_name)
 {
-    function onError(xhr, ajaxOptions, thrownError) {
-        toast(`Can't load matches for ${profile_name}`, TOAST_ERROR);
-        console.log(xhr.status);
-        console.log(thrownError);
-    }
-
-    function onSuccess(data)
+    if ($(`#${profile_name}-input`).is(":checked"))
     {
-        if ($(`#${profile_name}-input`).is(":checked"))
-        {
-            profile_activeHTML = `<div id=${profile_name}-last-match` 
-            profile_activeHTML += `class="profile_item"`
-            profile_activeHTML += `title="${profile_name}">KILLS: ${data.match.kills}`
-            profile_activeHTML += `</div>`
-            
-            $('#profile-info-div').html(profile_activeHTML)
-            $('#profile-info-div').show("slow")
-        } else {
-            profile_activeHTML = `inactive` 
-            $('#profile-info-div').hide("slow")
-        }
+        $(`#divTableRow_${profile_name}`).show("slow")
+    } else {
+        $(`#divTableRow_${profile_name}`).hide("slow")
     }
-    
-    $.get({
-        url: `api/v1/${profile_name}/match/last`,
-        dataType: 'json',
-        error: onError,
-        success: onSuccess
-      });
 }
 
 function loadConfiguredProfiles() {
@@ -68,20 +45,38 @@ function loadConfiguredProfiles() {
     }
 
     function onSuccess(data) {
+        // prepare html for images
         profilesHTML = ""
         imagesHTML = ""
         for (profile of data.profiles) {
             imagesHTML += `<div id="image-stats-${profile.username}-div" class="profile-stats-images"></div>`
             
-            profilesHTML += `<input type="checkbox" name="profiles" id="${profile.username}-input" onClick="onActivateProfile('${profile.username}')">`
+            profilesHTML += `<input type="checkbox" name="profiles" id="${profile.username}-input" onClick="onSelectProfile('${profile.username}')">`
             profilesHTML += `<label for="${profile.username}-input" >`  
             profilesHTML += `${profile.username}`
             profilesHTML += `</label>`
         }
         $(`#profile-stats-images-div`).html(imagesHTML)
 
+        // prepare html for sessions table
+        sessionsHTML = `<div class="divTableHeading">
+                            <div class="divTableCell">Name</div>
+                            <div class="divTableCell">Game Mode</div>
+                            <div class="divTableCell">Matches</div>
+                            <div class="divTableCell">Kills</div>
+                            <div class="divTableCell">Kill ratio</div>
+                            <div class="divTableCell">Eskores</div>
+                            <div class="divTableCell">Eskores ratio</div>
+                            <div class="divTableCell">trnRating diff</div>
+                            <div class="divTableCell">Best Match Kills</div>
+                        </div>`
+        $('#sessions-info-div').html(sessionsHTML)
+
+        i=0
         for (profile of data.profiles) {
-            loadImagesOfProfile(profile.username);
+            loadSessionsOfProfile(profile.username, i);
+            i++;
+            //loadImagesOfProfile(profile.username);
         }
         
         $('#profiles-div').html(profilesHTML)
@@ -94,6 +89,43 @@ function loadConfiguredProfiles() {
       error: onError,
       success: onSuccess
     });
+}
+
+function loadSessionsOfProfile(profile_name, index) {
+    function onSuccess(data) {
+        sessionsHTML = $('#sessions-info-div').html()
+        sessionsHTML += `<div class="divTableBody"><div class="divTableRow" id="divTableRow_${profile_name}">`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].username}</div>`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].game_mode}</div>`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].total_matches}</div>`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].total_kills}</div>`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].kill_ratio.toFixed(2)}</div>`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].total_eskores}</div>`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].eskores_ratio.toFixed(2)}</div>`
+        sessionsHTML += `<div class="divTableCell">${(data.session[0].last_trn_rating - data.session[0].first_trn_rating).toFixed(2)}</div>`
+        sessionsHTML += `<div class="divTableCell">${data.session[0].best_match}</div>`
+        sessionsHTML += `</div></div>`
+
+
+        $('#sessions-info-div').html(sessionsHTML)
+        //$(`#${profile_name}-input`).prop('checked', false);
+        if (index > 0) {
+            $(`#divTableRow_${profile_name}`).hide()
+        }
+    }
+
+    function onError(xhr, ajaxOptions, thrownError) {
+        toast(`Can't load sessions of ${profile_name}`, TOAST_ERROR);
+        console.log(xhr.status);
+        console.log(thrownError);
+    }
+
+    $.get({
+        url: `api/v1/${profile_name}/session/last?n=3`,
+        dataType: 'json',
+        error: onError,
+        success: onSuccess
+    })
 }
 
 function loadImagesOfProfile(profile_name) {
