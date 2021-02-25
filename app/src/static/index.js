0c4f5d7bac29ee4ce807751a045ac4e08115ce30
Guillermo Escudero <guillermoescudero@dorna.com>
2021-02-25 11:11:22 +0100

    frontend design changes

--- app/src/static/index.js
+++ app/src/static/index.js
@@ -1,7 +1,8 @@
 window.onload = function() {
-    setupUI();
-    loadConfiguredProfiles();
+    //setupUI();
+    loadConfiguredProfiles(true);
     loadBackground();
+    startTimer();
 };
 
 function loadBackground() {  
@@ -17,26 +18,28 @@ function loadBackground() {
     $('.mainDiv').css('background-image', `url("${$('#bg_canvas').getCanvasImage()}")`);
 }
 
-function setupUI() {
-    $( "#sample-button" ).click(onSampleButton);
-    $( ".profile-button" ).click(onSampleButton);
-}
+// function setupUI() {
+//     $( "#sample-button" ).click(onSampleButton);
+//     $( ".profile-button" ).click(onSampleButton);
+// }
 
-function onSampleButton() {
-    toast("Sample button pressed", TOAST_OK);
-}
+// function onSampleButton() {
+//     toast("Sample button pressed", TOAST_OK);
+// }
 
 function onSelectProfile(profile_name)
 {
     if ($(`#${profile_name}-input`).is(":checked"))
     {
         $(`#divTableRow_${profile_name}`).show("slow")
+        $(`#divTable_${profile_name}`).show("slow");
     } else {
         $(`#divTableRow_${profile_name}`).hide("slow")
+        $(`#divTable_${profile_name}`).hide("slow");
     }
 }
 
-function loadConfiguredProfiles() {
+function loadConfiguredProfiles(is_first_load) {
 
     function onError(xhr, ajaxOptions, thrownError) {
         toast(`Can't load profiles`, TOAST_ERROR);
@@ -45,20 +48,23 @@ function loadConfiguredProfiles() {
     }
 
     function onSuccess(data) {
-        // prepare html for images
-        profilesHTML = ""
-        imagesHTML = ""
-        for (profile of data.profiles) {
-            imagesHTML += `<div id="image-stats-${profile.username}-div" class="profile-stats-images"></div>`
-            
-            profilesHTML += `<input type="checkbox" name="profiles" id="${profile.username}-input" onClick="onSelectProfile('${profile.username}')">`
-            profilesHTML += `<label for="${profile.username}-input" >`  
-            profilesHTML += `${profile.username}`
-            profilesHTML += `</label>`
+        if (is_first_load) {
+            // prepare html for images
+            profilesHTML = ""
+            imagesHTML = ""
+            for (profile of data.profiles) {
+                imagesHTML += `<div id="image-stats-${profile.username}-div" class="profile-stats-images"></div>`
+                
+                profilesHTML += `<input type="checkbox" name="profiles" id="${profile.username}-input" onClick="onSelectProfile('${profile.username}')">`
+                profilesHTML += `<label for="${profile.username}-input" >`  
+                profilesHTML += `${profile.username}`
+                profilesHTML += `</label>`
+            }
+            $(`#profile-stats-images-div`).html(imagesHTML)
+            $('#profiles-div').html(profilesHTML)
+    
         }
-        $(`#profile-stats-images-div`).html(imagesHTML)
-
-        // prepare html for sessions table
+        // prepare html for sessions table (clean in reloads)
         sessionsHTML = `<div class="divTableHeading">
                             <div class="divTableCell">Name</div>
                             <div class="divTableCell">Game Mode</div>
@@ -71,16 +77,16 @@ function loadConfiguredProfiles() {
                             <div class="divTableCell">Best Match Kills</div>
                         </div>`
         $('#sessions-info-div').html(sessionsHTML)
+        // prepare html for matches table (clean in reloads)
+        matchesHTML = ``
+        $('#matches-info-div').html(matchesHTML)
 
         i=0
         for (profile of data.profiles) {
-            loadSessionsOfProfile(profile.username, i);
+            loadSessionsOfProfile(profile.username, i, is_first_load);
             i++;
             //loadImagesOfProfile(profile.username);
         }
-        
-        $('#profiles-div').html(profilesHTML)
-        
     }
 
     $.get({
@@ -91,26 +97,26 @@ function loadConfiguredProfiles() {
     });
 }
 
-function loadSessionsOfProfile(profile_name, index) {
+function loadSessionsOfProfile(profile_name, index, is_first_load) {
     function onSuccess(data) {
-        sessionsHTML = $('#sessions-info-div').html()
-        sessionsHTML += `<div class="divTableBody"><div class="divTableRow" id="divTableRow_${profile_name}">`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].username}</div>`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].game_mode}</div>`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].total_matches}</div>`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].total_kills}</div>`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].kill_ratio.toFixed(2)}</div>`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].total_eskores}</div>`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].eskores_ratio.toFixed(2)}</div>`
-        sessionsHTML += `<div class="divTableCell">${(data.session[0].last_trn_rating - data.session[0].first_trn_rating).toFixed(2)}</div>`
-        sessionsHTML += `<div class="divTableCell">${data.session[0].best_match}</div>`
-        sessionsHTML += `</div></div>`
-
-
-        $('#sessions-info-div').html(sessionsHTML)
-        //$(`#${profile_name}-input`).prop('checked', false);
-        if (index > 0) {
-            $(`#divTableRow_${profile_name}`).hide()
+        lastSessionHTML = getLastSessionTable(data);
+        matchesHTML = getLastMatchesTable(data);
+        
+        $('#sessions-info-div').html(lastSessionHTML);
+        $('#matches-info-div').html(matchesHTML);
+        
+        if (is_first_load) {
+            if (index > 0) {
+                $(`#divTableRow_${profile_name}`).hide();
+                $(`#divTable_${profile_name}`).hide();
+            } else {
+                $(`#${profile_name}-input`).prop('checked', true);
+            }
+        } else {
+            if (!$(`#${profile_name}-input`).is(":checked")) {
+                $(`#divTableRow_${profile_name}`).hide();
+                $(`#divTable_${profile_name}`).hide();
+            }
         }
     }
 
@@ -128,22 +134,99 @@ function loadSessionsOfProfile(profile_name, index) {
     })
 }
 
-function loadImagesOfProfile(profile_name) {
-    function onSuccess(data) {
-        imageProfileHTML = `<img src="${data.profile.stats_image}" width="380">`
-        $(`#image-stats-${profile_name}-div`).html(imageProfileHTML)
-    }
+// function loadImagesOfProfile(profile_name) {
+//     function onSuccess(data) {
+//         imageProfileHTML = `<img src="${data.profile.stats_image}" width="380">`
+//         $(`#image-stats-${profile_name}-div`).html(imageProfileHTML)
+//     }
 
-    function onError(xhr, ajaxOptions, thrownError) {
-        toast(`Can't load picture`, TOAST_ERROR);
-        console.log(xhr.status);
-        console.log(thrownError);
-    }
+//     function onError(xhr, ajaxOptions, thrownError) {
+//         toast(`Can't load picture`, TOAST_ERROR);
+//         console.log(xhr.status);
+//         console.log(thrownError);
+//     }
 
-    $.get({
-        url: `api/v1/${profile_name}/profile`,
-        dataType: 'json',
-        error: onError,
-        success: onSuccess
-    });
+//     $.get({
+//         url: `api/v1/${profile_name}/profile`,
+//         dataType: 'json',
+//         error: onError,
+//         success: onSuccess
+//     });
+// }
+
+function startTimer() {
+    var count = 180;
+    var counter = setInterval(timer, 1000);
+
+    function timer() {
+        count--;
+        if (count <= 0) {
+            //clearInterval(counter);
+            count = 180;
+            //timer is over
+            loadConfiguredProfiles(false);
+            return;
+        }
+        // mostramos el tiempo que queda hasta el siguiente refresco
+        //$(`#matches-info-div`).html(count)
+    }
+}
+
+function getLastSessionTable(profiles_data) {
+    sessionsHTML = $('#sessions-info-div').html();
+    sessionsHTML += `<div class="divTableBody"><div class="divTableRow" id="divTableRow_${profiles_data.session[0].username}">`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].username}</div>`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].game_mode}</div>`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].total_matches}</div>`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].total_kills}</div>`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].kill_ratio.toFixed(2)}</div>`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].total_eskores}</div>`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].eskores_ratio.toFixed(2)}</div>`;
+    sessionsHTML += `<div class="divTableCell">${(profiles_data.session[0].last_trn_rating - profiles_data.session[0].first_trn_rating).toFixed(2)}</div>`;
+    sessionsHTML += `<div class="divTableCell">${profiles_data.session[0].best_match}</div>`;
+    sessionsHTML += `</div></div>`;
+    return sessionsHTML;
+}
+
+function getLastMatchesTable(profiles_data) {
+    matchesHTML = $('#matches-info-div').html();
+    matchesHTML += `<div id="divTable_${profiles_data.session[0].username}">`
+    matchesHTML += `<div class="divTitleTable">${profiles_data.session[0].username}</div>`
+    matchesHTML += `<div class="divTable steelBlueCols" >
+                    <div class="divTableRow divTableSemiHeading">
+                    <div class="divTableCell"></div>
+                    <div class="divTableCell"></div>
+                    <div class="divTableCell">Mode</div>
+                    <div class="divTableCell">Matches</div>
+                    <div class="divTableCell">Kills</div>
+                    <div class="divTableCell">Eskores</div>
+                    <div class="divTableCell">TrnRating</div>
+                    </div>
+                    <div class="divTableBody">`
+    for (entry of profiles_data.session[0].entries) {
+        var match_date = new Date(entry.date_collected)
+        var diff = getDifferenceBetweenDates( match_date);
+        var match_top = getMatchTop(entry);
+        matchesHTML += `<div class="divTableRow">`;
+        matchesHTML += `<div class="divTableCell">${match_top}</div>`;
+        matchesHTML += `<div class="divTableCell"> ${diff}</div>`;
+        matchesHTML += `<div class="divTableCell">${entry.game_mode}</div>`;
+        matchesHTML += `<div class="divTableCell">${entry.matches}</div>`;
+        matchesHTML += `<div class="divTableCell">${entry.kills}</div>`;
+        matchesHTML += `<div class="divTableCell">${entry.eskores}</div>`;
+        matchesHTML += `<div class="divTableCell">${entry.trn_Rating}</div>`;
+        matchesHTML += `</div>`
+    }
+    matchesHTML += `</div></div></div>`;
+    return matchesHTML;
+}
+
+function getDifferenceBetweenDates(match_date) {
+    retval = "6 minutes ago";
+    return retval;
+}
+
+function getMatchTop(entry) {
+    retval = "Victory";
+    return retval;
 }
